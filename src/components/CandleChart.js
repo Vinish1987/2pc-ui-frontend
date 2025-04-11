25d50497176ab3e7651ebdf6f5217bcbf501804d
Vinish1987 <vini12345@gmail.com>
2025-04-11 09:22:26 +0530

    Update CandleChart.js

--- src/components/CandleChart.js
+++ src/components/CandleChart.js
@@ -2,28 +2,20 @@ import React, { useEffect, useRef } from 'react';
 import { createChart } from 'lightweight-charts';
 
 const CandleChart = () => {
-  const chartContainerRef = useRef(null);
+  const chartContainerRef = useRef();
 
   useEffect(() => {
     const chart = createChart(chartContainerRef.current, {
       width: 600,
       height: 400,
-      layout: {
-        background: { color: '#ffffff' },
-        textColor: '#000',
-      },
-      grid: {
-        vertLines: { color: '#eee' },
-        horzLines: { color: '#eee' },
-      },
     });
 
     const candleSeries = chart.addCandlestickSeries();
 
     candleSeries.setData([
-      { time: '2024-04-08', open: 100, high: 110, low: 90, close: 105 },
-      { time: '2024-04-09', open: 106, high: 115, low: 102, close: 110 },
-      { time: '2024-04-10', open: 108, high: 120, low: 100, close: 107 },
+      { time: '2022-04-01', open: 100, high: 110, low: 90, close: 105 },
+      { time: '2022-04-02', open: 106, high: 115, low: 95, close: 100 },
+      { time: '2022-04-03', open: 101, high: 120, low: 100, close: 115 },
     ]);
 
     return () => chart.remove();
